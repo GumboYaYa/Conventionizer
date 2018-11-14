7d4b1080da2d77655a9ae4605d61cfedd98014cc
Tom Lippitsch <t.lippitsch@northworks.de>
2018-11-14 23:50:18 +0100

    added lowercase loop

--- conventionizer.py
+++ conventionizer.py
@@ -1,8 +1,13 @@
 import os
 
-print(os.getcwd())
+# print(os.getcwd())
 
-for root, dirs, files in os.walk('./'):
+for root, dirs, files in os.walk('test_env'):
     print('Root path : ', root)
     print('Directories : ', dirs)
     print('Files : ', files)
+    for d in dirs:
+        if d[:].isupper():
+            dlow = d[:].lower()
+            os.rename(d, dlow)
+            print('Set to lowercase: {0} > {1}'.format(d, dlow))
