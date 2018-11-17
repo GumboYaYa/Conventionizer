9630bfeaa73e57bb9903848c0a2f2a1dccea6bf1
Tom Lippitsch <t.lippitsch@northworks.de>
2018-11-17 14:35:26 +0100

    uppercase detection works now for dirs and files

--- conventionizer.py
+++ conventionizer.py
@@ -4,19 +4,22 @@ import os
 
 for root, dirs, files in os.walk('test_env'):
 
-    print('Root path : ', root)
+    '''print('Root path : ', root)
     print('Directories : ', dirs)
-    print('Files : ', files)
+    print('Files : ', files)'''
+
+    # Set all folder and file names to lowercase.
 
     for d in dirs:
-        if d[:].isupper():
+        # any returns True if upper case letter is on string
+        if any(d.isupper() for d in d):
             d_source = os.path.join(root, d)
             d_lower = os.path.join(root, d[:].lower())
             os.rename(d_source, d_lower)
             print('Set folder to lowercase: {0} > {1}'.format(d_source, d_lower))
 
     for f in files:
-        if f[:].isupper():
+        if any(f.isupper() for f in f):
             f_source = os.path.join(root, f)
             f_lower = os.path.join(root, f[:].lower())
             os.rename(f_source, f_lower)
