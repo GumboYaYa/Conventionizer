0db4e96ea58f8eed8c5ca314936da24b3e4f7bb0
GumboYaYa <throbberwocky@gmail.com>
2019-04-19 23:36:22 +0200

    Partly added path validation Added directory filtering

--- conventionizer.py
+++ conventionizer.py
@@ -3,16 +3,53 @@ import pyperclip
 
 
 # print(os.getcwd())
-basedir = os.getcwd()
 
-if pyperclip.paste():
-    basedir = pyperclip.paste()
+#TODO Get the target path validation working
+'''
+basedir = ''
+clipboard = pyperclip.paste()
+
+
+def valid_path(path):
+    if os.path.exists(path):
+        global basedir
+        basedir = path
+
+
+if os.path.exists(clipboard):
+    basedir = clipboard
+else:
+    print('Your clipboard is either empty or not a valid path.')
+    while not os.path.exists(input()):
+        print('Please enter a valid path:')
+    basedir = input()
+
+print('The following directory has been set as target directory:')
+print(basedir)
+'''
+
+basedir = os.getcwd()
+folders = []
+files = []
 
 for root, dirs, files in os.walk(basedir):
+    d_index = 0
+    for d in dirs:
+        if d[0] == '.':
+            del dirs[d_index]
+        else:
+            folders.append(d)
+        d_index += 1
 
-    '''print('Root path : ', root)
-    print('Directories : ', dirs)
-    print('Files : ', files)'''
+print('Folders:')
+print(folders)
+
+'''
+for root, dirs, files in os.walk(basedir):
+
+    #print('Root path : ', root)
+    #print('Directories : ', dirs)
+    #print('Files : ', files)
 
     # Set all folder and file names to lowercase.
 
@@ -30,3 +67,15 @@ for root, dirs, files in os.walk(basedir):
             f_lower = os.path.join(root, f[:].lower())
             os.rename(f_source, f_lower)
             print('Set file to lowercase: {0} > {1}'.format(f_source, f_lower))
+
+
+for entry in os.scandir(basedir):
+    if entry.is_dir():
+        folders.append(entry.path)
+    if entry.is_file():
+        files.append(entry.path)
+
+print('Folders:')
+print(folders)
+'''
+
